e705e0920c07a594fe6a90aaae63a5c2679ce354
mrsohnbit@gmail.com <mmrsohn@gmail.com>
2024-07-19 10:04:51 +0900

    리스트 형태 변경

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2139785146"
+    serviceWorkerVersion: "1598017380"
   }
 });
