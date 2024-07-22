5a9a64362c72c5a8a481d23d7637d1013d254c66
mrsohnHome <mmrsohn@gmail.com>
2024-07-22 22:09:04 +0900

    컴플릿트 처리??

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2894129492"
+    serviceWorkerVersion: "3317378759"
   }
 });
